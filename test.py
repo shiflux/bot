1875fdcdee5a95bf632be845a10305b668ea261e
shiflux <artem.piazzi1@stud.unifi.it>
2016-02-28 04:47:31 +0100

    Added class bot Created file test for testing purposes

--- test.py
+++ test.py
@@ -8,23 +8,26 @@ def test(b):
         b.methods.send_pass()
         b.methods.send_nick()
         b.methods.join_channel()
-        b.methods.send_message("hello")
+        #b.methods.send_message("MingLee")
+        counter = 0
         while True:
-            response = ""
-            response = response+b.sock.recv(1024).decode('UTF-8')
+            # response = ""
+            # response = response+b.sock.recv(1024).decode('UTF-8')
+            # response_split = re.split(r"[~\r\n]+", response)
+            # response = response_split.pop()
+            #
+            # for line in response_split:
+            #         line = str.rstrip(line)
+            #         line = str.split(line)
+            #
+            #         if len(line) >= 1:
+            #             if line[0] == 'PING':
+            #                 b.methods.send_pong(line[1])
+            if(counter<150):
+                b.methods.send_message("MingLee "+str(counter))
+                counter+=1
 
-            response_split = re.split(r"[~\r\n]+", response)
-            response = response_split.pop()
-
-            for line in response_split:
-                    line = str.rstrip(line)
-                    line = str.split(line)
-
-                    if len(line) >= 1:
-                        if line[0] == 'PING':
-                            b.methods.send_pong(line[1])
-
-            time.sleep(1 / cfg_old.RATE)
+            time.sleep(0.1)
 
 b = bot.Bot(2)
 test(b)
